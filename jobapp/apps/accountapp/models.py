7a915a9567316f9c809048dff7080aeaa85e7f50
Isagani Lapira <lapiraisagani@gmail.com>
2023-11-18 10:30:29 +0800

    Adding two more apps: core and management

--- jobapp/apps/accountapp/models.py
+++ jobapp/apps/accountapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# user table
+class User(models.Model):
+    email = models.EmailField(max_length=255)
+    password = models.TextField()
+    isActivated = models.BooleanField(default=True)
+    
+
+class Person(models.Model):
+    SEX_CHOICE = [('M','Male'),('F','Female')]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    birthdate = models.DateField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICE,blank=True)
+    street_address = models.CharField(max_length=100)
+    apartment_no = models.IntegerField(null=True)
+    city = models.CharField(max_length=70)
+    postal_code = models.IntegerField()
+    
+    
+class Alerts(models.Model):
+    NOTIF_ACTION = [('Applicant','New Applicant Applied to your post'),('MatchSkill','Job matches your skill')]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    
+    
--- jobapp/apps/core/apps.py
+++ jobapp/apps/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccountappConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.accountapp'
+    name = 'core'
