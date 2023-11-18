302dec9adefe7519cff8a8b178fbbe7828bed1e5
Isagani Lapira <lapiraisagani@gmail.com>
2023-11-18 14:38:18 +0800

    Compleeting initial models

--- jobapp/apps/profileapp/models.py
+++ jobapp/apps/profileapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from apps.accountapp.models import User
+from apps.jobsapp.models import Job
 
-# Create your models here.
+class JobSeeker(models.Model):
+    qualification = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    experience = models.TextField()
+    skills = models.TextField()
+    
+    
+    
+class JobApplication(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('deleted','Deleted')
+    ]
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    # foreign keys
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job_seeker = models.ForeignKey(JobSeeker,on_delete=models.CASCADE)
+    
+    
