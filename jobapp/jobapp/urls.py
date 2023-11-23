433b135dd11fb0f74dcfd8707850409cf77901d9
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-11-23 23:08:09 +0800

    chore: installed django-tailwind

--- jobapp/jobapp/urls.py
+++ jobapp/jobapp/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("job/", include(jobs_urls)),
     path("management/", include(management_urls)),
     path("profile/", include(profile_urls)),
+    # EXTRAS / LIB
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
