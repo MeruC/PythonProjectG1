17b0c212729fd13f302a4ee13288c1afa206fcaf
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2023-11-23 21:59:30 +0800

    add: app name

--- jobapp/apps/accountapp/urls.py
+++ jobapp/apps/accountapp/urls.py
@@ -1,7 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+app_name = "accountapp"
 
 urlpatterns = [
     path("register/", views.Register, name="register"),
