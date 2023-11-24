316c8fb5a90c58f8eeb56cfd0d1ce912f5823d88
Isagani Lapira <lapiraisagani@gmail.com>
2023-11-24 20:32:32 +0800

    remove: app_name conflicting in other apps

--- jobapp/apps/profileapp/urls.py
+++ jobapp/apps/profileapp/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from . import views
 
 
-app_name = "profileapp"
-
 urlpatterns = [
     path("", views.index, name="index"),
     path('addWorkExp/', views.addWorkExp, name="addWorkExp"), #add work experience
     path('addEducation/',views.addEducation, name="addEducation"),
+    path('delWork/<int:id>/',views.delete_work,name="deleteWork"),
     # todo update this later since there's no view for logout yet
     path("logout/", views.index, name="logout"),
 ]
