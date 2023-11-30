8d5a4432ab907051801b99d71dfe6780e2cfe79d
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2023-11-30 21:09:58 +0800

    change: make a new template for manage users

--- jobapp/apps/management/views.py
+++ jobapp/apps/management/views.py
@@ -23,3 +23,7 @@ def my_login_view(request):
 
 def index(request):
     return render(request, "management/dashboard.html")
+
+
+def manage_users(request):
+    return render(request, "management/manage_users.html")
