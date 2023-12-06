43fe19b96e127710059b729497e625d66ae7fc5d
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2023-12-06 13:48:00 +0800

    remove: todo

--- jobapp/apps/management/views.py
+++ jobapp/apps/management/views.py
@@ -1,24 +1,4 @@
-from django.shortcuts import redirect, render
-
-from django.contrib.auth import authenticate, login
-
-
-# todo to be used when they logged in as admin. should move this to account
-def my_login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        if user.is_staff:
-            redirect("managementapp:index")
-
-        else:
-            # Redirect to a success page.
-            ...
-    else:
-        # Return an 'invalid login' error message.
-        ...
+from django.shortcuts import render
 
 
 def index(request):
