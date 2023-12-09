0568a4492bc0bad7f02a3bf488ef5ca115e7281c
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2023-12-09 10:06:39 +0800

    add: datatables

--- jobapp/apps/management/static/management/manage-users/history.js
+++ jobapp/apps/management/static/management/manage-users/history.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#user-table").DataTable({
+  $("#history-table").DataTable({
     lengthMenu: [], // Remove the entries per page dropdown
     language: {
       lengthMenu: " ", // Hide the "Show [X] entries" text
