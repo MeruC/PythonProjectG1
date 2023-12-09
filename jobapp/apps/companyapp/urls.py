eea59ea1104ce7fdfd628d4877afb1f71f0fcdd4
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2023-12-09 14:41:34 +0800

    chore:setup create job html

--- jobapp/apps/companyapp/urls.py
+++ jobapp/apps/companyapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # TODO: change this to dynamic url
     path("companyId/", views.companyProfile, name="companyProfile"),
     path("myCompany/jobListings", views.companyJobList, name="companyJobList"),
+    path("myCompany/jobListings/createJob", views.createJob, name="createJob"),
     path("myCompany/applicants", views.companyApplicants, name="companyApplicants"),
     path("myCompany/", views.companyProfileSettings, name="companyProfileSettings")
 ]
