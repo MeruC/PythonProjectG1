a076947ba61d5d8f2c03949e6d6124d6662fb1e0
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2023-12-10 04:08:00 +0800

    remove: redundant todo

--- jobapp/apps/management/views.py
+++ jobapp/apps/management/views.py
@@ -15,24 +15,7 @@ from apps.jobsapp.models import Job, Company
 
 from django.contrib import messages
 
-from .forms import  EditCompanyForm, EditCompanyImageForm, EditJobForm
-
-# todo to be used when they logged in as admin. should move this to account
-def my_login_view(request):
-    username = request.POST["username"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        if user.is_staff:
-            redirect("managementapp:index")
-
-        else:
-            # Redirect to a success page.
-            ...
-    else:
-        # Return an 'invalid login' error message.
-        ...
+from .forms import EditCompanyForm, EditCompanyImageForm, EditJobForm
 
 
 def index(request):
@@ -40,7 +23,6 @@ def index(request):
 
 
 def manage_users(request):
-
     User = get_user_model()
     normal_users = User.objects.filter(is_superuser=False)
 
@@ -192,8 +174,13 @@ def edit_work(request, work):
         work.work_title = request.POST.get("work_title")
         work.company_name = request.POST.get("company_name")
         work.position = request.POST.get("position")
-        work.start_date = f"{request.POST.get('started_month')}, {request.POST.get('started_year')}"
-        work.end_date = f"{request.POST.get('end_month')}, {request.POST.get('end_year')}"
+        work.start_date = (
+            f"{request.POST.get('started_month')},"
+            f" {request.POST.get('started_year')}"
+        )
+        work.end_date = (
+            f"{request.POST.get('end_month')}, {request.POST.get('end_year')}"
+        )
         work.save()
         messages.success(request, "work added successfully.")
     else:
@@ -280,11 +267,13 @@ def history(request, id):
         {"applications": application_list, "user": user},
     )
 
+
 # manage jobs ------------------------------
+
 
 def manage_jobs(request):
     jobs = Job.objects.all()
-    context = {'jobs': jobs}
+    context = {"jobs": jobs}
     return render(request, "management/manage-jobs/manage_jobs.html", context)
 
 
@@ -293,63 +282,63 @@ def edit_job(request, job_id):
         job = Job.objects.get(id=job_id)
     except Job.DoesNotExist:
         return redirect("managementapp:manage_jobs")
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = EditJobForm(request.POST)
         if form.is_valid():
             try:
-                job.job_title = form.cleaned_data['job_title']
-                job.description = form.cleaned_data['description']
-                job.type = form.cleaned_data['type']
-                job.skills = form.cleaned_data['skills']
+                job.job_title = form.cleaned_data["job_title"]
+                job.description = form.cleaned_data["description"]
+                job.type = form.cleaned_data["type"]
+                job.skills = form.cleaned_data["skills"]
 
                 job.save()
                 messages.success(
                     request,
                     "Job Updated Successfully",
-                ) 
+                )
             except Exception as e:
                 messages.error(request, "Internal Server Error")
                 print(e)
     else:
-        form = EditJobForm(initial={
-            'type': job.type,
-            'skills': job.skills,
-            'job_title': job.job_title,
-            'description': job.description,
-            
-        })
+        form = EditJobForm(
+            initial={
+                "type": job.type,
+                "skills": job.skills,
+                "job_title": job.job_title,
+                "description": job.description,
+            }
+        )
 
-    context = {'job': job, 'form': form}
-    return render(request, 'management/manage-jobs/job_edit.html', context)
-
+    context = {"job": job, "form": form}
+    return render(request, "management/manage-jobs/job_edit.html", context)
 
 
 def action_job(request, job_id):
-
     try:
-        job = Job.objects.get( id=job_id)
+        job = Job.objects.get(id=job_id)
     except Job.DoesNotExist:
         return redirect("managementapp:manage_jobs")
-    
-    if request.method == 'POST':
-        try :
-            if job.status == 'active':
-                job.status = 'deleted'
+
+    if request.method == "POST":
+        try:
+            if job.status == "active":
+                job.status = "deleted"
             else:
-                job.status = 'active'
+                job.status = "active"
             job.save()
             messages.success(
-                    request,
-                    "Job Activated Successfully" if job.status == 'active' else "Job Deleted Successfully",
-                ) 
+                request,
+                "Job Activated Successfully"
+                if job.status == "active"
+                else "Job Deleted Successfully",
+            )
         except Exception as e:
             messages.error(request, "Internal Server Error")
             print(e)
-  
-    context = {'job': job}
-    return render(request, 'management/manage-jobs/job_action.html', context)
 
+    context = {"job": job}
+    return render(request, "management/manage-jobs/job_action.html", context)
 
 
 def delete_job(request, job_id):
@@ -357,23 +346,26 @@ def delete_job(request, job_id):
         job = Job.objects.get(id=job_id)
         job.delete()
         messages.success(
-                    request,
-                    "Job  deleted successfully" ,
-                )
+            request,
+            "Job  deleted successfully",
+        )
     except Job.DoesNotExist:
         return redirect("managementapp:manage_jobs")
     except Exception as e:
-            messages.error(request, "Internal Server Error")
-            print(e)
+        messages.error(request, "Internal Server Error")
+        print(e)
     return redirect("managementapp:manage_jobs")
-    
-    
+
+
 # manage companies ------------------------------
+
 
 def manage_companies(request):
     companies = Company.objects.all()
-    context = {'companies': companies}
-    return render(request, "management/manage-companies/manage_companies.html", context)
+    context = {"companies": companies}
+    return render(
+        request, "management/manage-companies/manage_companies.html", context
+    )
 
 
 def edit_company(request, company_id):
@@ -381,86 +373,93 @@ def edit_company(request, company_id):
         company = Company.objects.get(id=company_id)
     except Company.DoesNotExist:
         return redirect("managementapp:manage_companies")
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = EditCompanyForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                company.company_name = form.cleaned_data['company_name']
-                company.description = form.cleaned_data['description']
-                company.country = form.cleaned_data['country']
-                company.city = form.cleaned_data['city']
-                if form.cleaned_data['logo']:
-                    company.logo = form.cleaned_data['logo']
+                company.company_name = form.cleaned_data["company_name"]
+                company.description = form.cleaned_data["description"]
+                company.country = form.cleaned_data["country"]
+                company.city = form.cleaned_data["city"]
+                if form.cleaned_data["logo"]:
+                    company.logo = form.cleaned_data["logo"]
 
                 company.save()
                 messages.success(
                     request,
                     "Company Updated Successfully",
-                ) 
+                )
             except Exception as e:
                 messages.error(request, "Internal Server Error")
                 print(e)
     else:
-        form = EditCompanyForm(initial={
-            'company_name': company.company_name,
-            'description': company.description,
-            'country': company.country,
-            'city': company.city,
-            'logo': company.logo,
-            
-        })
-        
+        form = EditCompanyForm(
+            initial={
+                "company_name": company.company_name,
+                "description": company.description,
+                "country": company.country,
+                "city": company.city,
+                "logo": company.logo,
+            }
+        )
 
-    context = {'company': company, 'form': form, 
+    context = {
+        "company": company,
+        "form": form,
+    }
+    return render(
+        request, "management/manage-companies/company_edit.html", context
+    )
 
-               }
-    return render(request, 'management/manage-companies/company_edit.html', context)
 
 def edit_company_image(request, company_id):
     try:
         company = Company.objects.get(id=company_id)
     except Company.DoesNotExist:
         return redirect("managementapp:manage_companies")
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = EditCompanyImageForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                company.logo = form.cleaned_data['logo']
+                company.logo = form.cleaned_data["logo"]
                 company.save()
             except Exception as e:
                 print(e)
-    
-    return redirect(request, 'managementapp:edit_company', company_id=company_id)
 
+    return redirect(
+        request, "managementapp:edit_company", company_id=company_id
+    )
 
 
 def action_company(request, company_id):
-
     try:
-        company = Company.objects.get( id=company_id)
+        company = Company.objects.get(id=company_id)
     except Company.DoesNotExist:
         return redirect("managementapp:manage_companies")
-    
-    if request.method == 'POST':
-        try :
+
+    if request.method == "POST":
+        try:
             if company.is_active == True:
                 company.is_active = False
             else:
                 company.is_active = True
             company.save()
             messages.success(
-                    request,
-                    "Company Activated Successfully" if company.is_active == True else "Company  Deleted Successfully", 
-                ) 
+                request,
+                "Company Activated Successfully"
+                if company.is_active == True
+                else "Company  Deleted Successfully",
+            )
         except Exception as e:
             messages.error(request, "Internal Server Error")
             print(e)
-  
-    context = {'company': company}
-    return render(request, 'management/manage-companies/company_action.html', context)
 
+    context = {"company": company}
+    return render(
+        request, "management/manage-companies/company_action.html", context
+    )
 
 
 def delete_company(request, company_id):
@@ -468,14 +467,12 @@ def delete_company(request, company_id):
         company = Company.objects.get(id=company_id)
         company.delete()
         messages.success(
-                    request,
-                    "Company  Deleted Successfully" ,
-                )
+            request,
+            "Company  Deleted Successfully",
+        )
     except Company.DoesNotExist:
         return redirect("managementapp:manage_companies")
     except Exception as e:
-            messages.error(request, "Internal Server Error")
-            print(e)
+        messages.error(request, "Internal Server Error")
+        print(e)
     return redirect("managementapp:manage_companies")
-    
-
