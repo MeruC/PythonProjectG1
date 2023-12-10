6fa34781bd660785197616a4d71d575e8676f326
darwin <darwinsanluisramos21@gmail.com>
2023-12-10 13:18:14 +0800

    fix: manage job bug

--- jobapp/static/js/navbar.js
+++ jobapp/static/js/navbar.js
@@ -1,18 +1,3 @@
-function onLogoutHandler() {
-  Swal.fire({
-    title: "Logout?",
-    text: "Are you sure you want to logout?",
-    icon: "warning",
-    confirmButtonText: "Logout",
-    confirmButtonColor: "#EF5350",
-    showCancelButton: true,
-    showCloseButton: false,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      window.location.href = "/account/logout/";
-    }
-  });
-}
 document
   .querySelector("#dropdownIcon")
   .addEventListener("click", function (event) {
