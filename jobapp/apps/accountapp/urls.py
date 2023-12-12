ff75b0d463791e445f8c9952859625f51b83422e
MeruC <carmelomelvincent@gmail.com>
2023-12-12 12:59:59 +0800

    Code Cleanup and Blank URL redirects

--- jobapp/apps/accountapp/urls.py
+++ jobapp/apps/accountapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "accountapp"
 urlpatterns = [
+    path("", views.index, name="index"),
     path("register/", views.Register, name="register"),
     path("login/", views.Login, name="login"),
     path("logout/", views.Logout, name="logout"),
--- jobapp/jobapp/urls.py
+++ jobapp/jobapp/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from apps.accountapp import urls as account_urls
-from apps.core import urls as core_urls
+#from apps.core import urls as core_urls
 from apps.jobsapp import urls as jobs_urls
 from apps.management import urls as management_urls
 from apps.profileapp import urls as profile_urls
@@ -14,7 +14,7 @@ urlpatterns = [
     # homepage
     path("", include((jobs_urls), namespace='home')),
     path("account/", include(account_urls)),
-    path("core/", include(core_urls)),
+    #path("core/", include(core_urls)),
     path("job/", include((jobs_urls), namespace='job')),
     path("management/", include(management_urls)),
     path("profile/", include(profile_urls)),
